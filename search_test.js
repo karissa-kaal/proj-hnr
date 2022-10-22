863af69299488b5fbb0065baec24f6a20e684278
Noureen Fatima <karissa.kaal@gmail.com>
2022-10-22 21:52:36 +0500

    Search Test Passed

--- search_test.js
+++ search_test.js
@@ -1,5 +1,9 @@
 Feature('search');
 
 Scenario('test something', ({ I }) => {
+    I.amOnPage('/');
+    I.click('.RSFSearchField-root-79');
+    I.click('Category 1');
+    I.see('Subcategory 1', 'h1');
 
 });
